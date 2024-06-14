6b0f3322723cb6fa6967f10cb37c10dda59e1a47
lariiskz <127518749+lariiskz@users.noreply.github.com>
2024-06-14 11:10:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
 
